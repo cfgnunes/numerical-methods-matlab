1eaa56aef7ebe651a37b58d0902680c659a9ac32
Cristiano Nunes <cfgnunes@gmail.com>
2017-10-25 02:31:24 -0200

    Added backward and forward substitution

---
+++ linear_systems/forward_substitution.m
@@ -0,0 +1,24 @@
+function [x] = forward_substitution(l, c)
+% Solve the lower linear system lx=c
+% Inputs:
+%         l: Lower triangular matrix
+%         c: Array containing c values
+% Outputs:
+%         x: Solution of linear system
+
+    n = size(u, 1);
+    m = size(u, 2);
+
+    if n ~= m
+        error('Error: "l" must be a square matrix.')
+    end
+
+    x(1) = c(1) / l(1, 1);
+    for i = 2:n
+        sum_x = 0;
+        for j = 1:(i-1)
+            sum_x = sum_x + l(i, j) * x(j);
+        end
+        x(i) = (c(i) - sum_x) / l(i, i);
+    end
+end
