5a44374cd722d7b17c8c40f7fdfdbdbcb9171885
Cristiano Nunes <cfgnunes@gmail.com>
2017-10-25 01:59:07 -0200

    Code Beautifier

--- main.m
+++ main.m
@@ -11,6 +11,7 @@ format short;
 addpath differentiation
 addpath integration
 addpath interpolation
+addpath linear_systems
 addpath ode
 addpath polynomials
 addpath solutions
@@ -25,8 +26,8 @@ addpath solutions
 %         It is necessary to enter a search interval [a, b];
 %         The interval reported must have a signal exchange, f (a) * f (b) <0.
 disp('Solutions: Bisection method');
-f = @(x) (4*x^3 + x + cos(x) - 10);
-tol = 10^-5;
+f = @(x) (4 * x ^ 3 + x + cos(x) - 10);
+tol = 10 ^ -5;
 iter_max = 100;
 a = 1.0;
 b = 2.0;
@@ -40,9 +41,9 @@ b = 2.0;
 %         It is necessary to calculate the derivative of the function;
 %         It is necessary to give an initial x0 value where f'(x0) must be nonzero.
 disp('Solutions: Newton method');
-f = @(x) (4*x^3 + x + cos(x) - 10);
-df =  @(x) (12 * x^2 + 1 - sin(x));
-tol = 10^-5;
+f = @(x) (4 * x ^ 3 + x + cos(x) - 10);
+df = @(x) (12 * x ^ 2 + 1 - sin(x));
+tol = 10 ^ -5;
 iter_max = 100;
 x0 = 2.0;
 [root, iter, converged] = newton(f, df, x0, tol, iter_max)
@@ -55,33 +56,33 @@ x0 = 2.0;
 %         It may diverge if the function is not approximately linear in the range containing the root;
 %         It is necessary to give two points 'a' and 'b' where f(a)-f(b) must be nonzero.
 disp('Solutions: Secant method');
-f = @(x) (4*x^3 + x + cos(x) - 10);
-tol = 10^-5;
+f = @(x) (4 * x ^ 3 + x + cos(x) - 10);
+tol = 10 ^ -5;
 iter_max = 100;
 a = 1.0;
 b = 2.0;
 [root, iter, converged] = secant(f, a, b, tol, iter_max)
 
 disp('Interpolation: Lagrange method');
-x=[2 11/4 4]
-y=[1/2 4/11 1/4]
-x_int=3
+x = [2 11 / 4 4]
+y = [1 / 2 4 / 11 1 / 4]
+x_int = 3
 [y_int] = lagrange(x, y, x_int)
 
 disp('Interpolation: Neville method');
-x=[1.0 1.3 1.6 1.9 2.2]
-y=[0.7651977 0.6200860 0.4554022 0.2818186 0.1103623]
-x_int=1.5
+x = [1.0 1.3 1.6 1.9 2.2]
+y = [0.7651977 0.6200860 0.4554022 0.2818186 0.1103623]
+x_int = 1.5
 [y_int, q] = neville(x, y, x_int)
 
 disp('Polynomials: Briot-Ruffini method');
-root=-2;
-a=[2 0 -3 3 -4]
+root = - 2;
+a = [2 0 - 3 3 - 4]
 [b, rest] = briot_ruffini(root, a)
 
 disp('Polynomials: Newtons Divided-Difference method');
-x=[1.0 1.3 1.6 1.9 2.2];
-y=[0.7651977 0.6200860 0.4554022 0.2818186 0.1103623];
+x = [1.0 1.3 1.6 1.9 2.2];
+y = [0.7651977 0.6200860 0.4554022 0.2818186 0.1103623];
 [f] = newton_divided_difference(x, y)
 
 disp('Differentiation: Backward-difference method')
@@ -96,37 +97,37 @@ y = [9.025013 11.02318 13.46374 16.44465]
 
 disp('Differentiation: Five-Point method')
 x = [2.1 2.2 2.3 2.4 2.5 2.6]
-y = [-1.709847 -1.373823 -1.119214 -0.9160143 -0.7470223 -0.6015966]
+y = [- 1.709847 - 1.373823 - 1.119214 - 0.9160143 - 0.7470223 - 0.6015966]
 [dy] = derivative_five_point(x, y)
 
 disp('Integration: Trapezoidal Rule')
-x=[0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84];
-y=[124, 134, 148, 156, 147, 133, 121, 109, 99, 85, 78, 89, 104, 116, 123];
+x = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84];
+y = [124, 134, 148, 156, 147, 133, 121, 109, 99, 85, 78, 89, 104, 116, 123];
 [xi] = composite2_trapezoidal(x, y)
 
 disp('Integration: Trapezoidal Rule')
-f = @(x) (x^2 * log(x^2 + 1));
+f = @(x) (x ^ 2 * log(x ^ 2 + 1));
 a = 0.0;
 b = 2.0;
 h = 0.25
-n = (b-a)/h;
+n = (b - a) / h;
 [xi] = composite_trapezoidal(f, b, a, n)
 
 disp('Integration: Composite 1/3 Simpsons Rule')
-x=[0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84];
-y=[124, 134, 148, 156, 147, 133, 121, 109, 99, 85, 78, 89, 104, 116, 123];
+x = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84];
+y = [124, 134, 148, 156, 147, 133, 121, 109, 99, 85, 78, 89, 104, 116, 123];
 [xi] = composite2_simpson(x, y)
 
 disp('Integration: Composite 1/3 Simpsons Rule')
-f = @(x) (x^2 * log(x^2 + 1));
+f = @(x) (x ^ 2 * log(x ^ 2 + 1));
 a = 0.0;
 b = 2.0;
 h = 0.25
-n = (b-a)/h;
+n = (b - a) / h;
 [xi] = composite_simpson(f, b, a, n)
 
 disp('EDO: Euler method')
-f = @(x, y) (y - x^2 + 1);
+f = @(x, y) (y - x ^ 2 + 1);
 a = 0.0;
 b = 2.0;
 n = 10;
@@ -134,8 +135,8 @@ ya = 0.5;
 [vx, vy] = euler(f, a, b, n, ya);
 
 disp('EDO: Taylor (Order Two) method')
-f = @(x, y) (y -x^2 +1);
-df1 = @(x, y) (y -x^2 +1 -2*x);
+f = @(x, y) (y - x ^ 2 + 1);
+df1 = @(x, y) (y - x ^ 2 + 1 - 2 * x);
 a = 0.0;
 b = 2.0;
 n = 10;
@@ -143,10 +144,10 @@ ya = 0.5;
 [vx, vy] = taylor2(f, df1, a, b, n, ya);
 
 disp('EDO: Taylor (Order Four) method')
-f = @(x, y) (y -x^2 +1);
-df1 = @(x, y) (y -x^2 +1 -2*x);
-df2 = @(x, y) (y -x^2 +1 -2*x -2);
-df3 = @(x, y) (y -x^2 +1 -2*x -2);
+f = @(x, y) (y - x ^ 2 + 1);
+df1 = @(x, y) (y - x ^ 2 + 1 - 2 * x);
+df2 = @(x, y) (y - x ^ 2 + 1 - 2 * x - 2);
+df3 = @(x, y) (y - x ^ 2 + 1 - 2 * x - 2);
 a = 0.0;
 b = 2.0;
 n = 10;
@@ -154,7 +155,7 @@ ya = 0.5;
 [vx, vy] = taylor4(f, df1, df2, df3, a, b, n, ya);
 
 disp('EDO: Runge-Kutta (Order Four) method')
-f = @(x, y) (y - x^2 + 1);
+f = @(x, y) (y - x ^ 2 + 1);
 a = 0.0;
 b = 2.0;
 n = 10;
@@ -164,13 +165,22 @@ ya = 0.5;
 disp('EDO: Runge-Kutta (Order Four) method for systems of differential equations')
 m = 2;
 f = cell(m, 1);
-f{1} = @(x, y) (-4*y(1) + 3*y(2) + 6);
-f{2} = @(x, y) (-2.4*y(1) + 1.6*y(2) + 3.6);
+f{1} = @(x, y) (- 4 * y(1) + 3 * y(2) + 6);
+f{2} = @(x, y) (- 2.4 * y(1) + 1.6 * y(2) + 3.6);
 a = 0.0;
 b = 0.5;
 h = 0.1;
-n = (b-a)/h;
+n = (b - a) / h;
 ya = zeros(m, 1);
 ya(1) = 0.0;
 ya(2) = 0.0;
 [vx, vy] = rk4_system(f, a, b, n, ya)
+
+%disp('Linear Systems: Gaussian Elimination')
+%A = [1 1 0 3; 2 1 - 1 1; 3 - 1 - 1 2; - 1 2 3 - 1]
+%b = [4 1 - 3 4]
+%
+%disp('Linear Systems: Backward Substitution')
+%U = [1 - 1 2 - 1; 0 2 - 1 1; 0 0 - 1 - 1; 0 0 0 2];
+%d = [- 8 6 - 4 4];
+%[x] = backward_substitution(U, d)
