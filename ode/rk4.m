b952fba28e1db152d0d1613a6605d8d0330d78c4
HadesDMen <82605894+HadesDMen@users.noreply.github.com>
2021-11-17 11:41:50 +0530

    Update rk4.m
    May I enter the dragon...
    First of all, thanks for the awesome repo, mate.
    I am new to coding and a student at the moment. So in k1, k2, k3, k4, there is an error, and most websites have made the same mistake.  Everyone has actually missed an 'h' term in the function as you can notice.
    Regards
    HadesDMen
    DMen leaves the chat

--- ode/rk4.m
+++ ode/rk4.m
@@ -24,9 +24,9 @@ function [vx, vy] = rk4(f, a, b, n, ya)
 
     for i = 1:n
         k1 = h * f(x, y);
-        k2 = h * f(x + h / 2, y + k1 / 2);
-        k3 = h * f(x + h / 2, y + k2 / 2);
-        k4 = h * f(x + h, y + k3);
+        k2 = h * f(x + h / 2, y + k1 * h/ 2);
+        k3 = h * f(x + h / 2, y + k2 * h/ 2);
+        k4 = h * f(x + h, y + k3 * h);
 
         x = a + i * h;
         y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
